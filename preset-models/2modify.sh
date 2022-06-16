19468a3bb2318d10639841cfddde1f6c133ded89
HongbinZheng <myspoto@gmail.com>
2022-06-16 14:35:43 +0800

    Update 2modify.sh

--- preset-models/2modify.sh
+++ preset-models/2modify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #修改登录IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.218.254/g' package/base-files/files/bin/config_generate
 #修改主机名
 sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
 #删除自带低版本xray-core
@@ -10,7 +10,7 @@ rm -rf package/feeds/packages/xray-core
 mkdir -p files/etc/uci-defaults
 cp -f uci-scripts/* files/etc/uci-defaults
 #复制内核5.10版本CPU超频补丁
-#\cp -rf extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+cp -rf extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 #设置WIFI
 #sed -i 's/OpenWrt/coolxiaomi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -18,7 +18,7 @@ cp -f uci-scripts/* files/etc/uci-defaults
 #修改登录密码为coolxiaomi
 #sed -i '/root/croot:$1$CBd7u73H$LvSDVXLBrzpk4JfuuN.Lv1:18676:0:99999:7:::' package/base-files/files/etc/shadow
 #切换ramips内核到5.15
-#sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' target/linux/ramips/Makefile
+sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' target/linux/ramips/Makefile
 
 #删除一些zzz中的脚本
 sed -i '/DISTRIB_/d' package/default-settings/files/zzz-default-settings
