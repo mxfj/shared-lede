0438a997c17e52fc4cc0db409e4aacff1df3b329
HongbinZheng <myspoto@gmail.com>
2022-06-16 15:15:47 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -5,10 +5,6 @@ git clone -b master --single-branch https://github.com/coolsnowwolf/lede
 mv ./openwrt/* ./
 
 # 下载自定义插件
-cd package/lean
-rm -rf luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-cd ../..
 mkdir -p package/myapps
 cd package/myapps
 git clone https://github.com/jerrykuku/luci-theme-argon.git
